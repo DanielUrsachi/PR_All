a5e903dc86551123d038700a430fc96e531d53d4
Daniel Ursachi <udanny95@gmail.com>
2017-05-25 02:04:25 +0300

    add lab7

---
+++ l7/src/Show.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+/**
+ * Created by Dan on 25-May-17.
+ */
+public class Show {
+    private static JTextArea label;
+    public static JFrame frame;
+
+    private static void createAndShowGUI() {
+         frame = new JFrame("Stream");
+
+         label = new JTextArea("This is a Swing frame");
+        label.setText("Wait connection");
+        label.setSize(600,600);
+
+        frame.add(label);
+
+        frame.setSize(600, 600); // width=350, height=200
+        frame.setVisible(true); // Display the frame
+    }
+    public static void setString(String s){
+
+        label.setText(s);
+        //frame.add(label);
+    }
+    public static void createApp(){
+        createAndShowGUI();
+    }
+    public static void main(String[] args) throws AWTException, IOException {
+        createAndShowGUI();
+        setString("ss");
+
+    }
+}
